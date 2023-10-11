fdda78981ff2fa3631e4e4d5505d47e59e150a34
Mike Smithson <mikesmithso@gmail.com>
2023-10-11 19:31:54 -0400

    stair climbing

--- src/main/java/com/leetcode/StairClimbing.java
+++ src/main/java/com/leetcode/StairClimbing.java
@@ -2,28 +2,24 @@ package com.leetcode;
 
 public class StairClimbing {
 
-    public int climbStairs(int numberOfStairs) {
-        int[] memo = new int[numberOfStairs + 1];
+    public long climbStairs(int numberOfStairs) {
+        long[] memo = new long[numberOfStairs + 1];
         return recursivelyClimbStairs(0, numberOfStairs, memo);
     }
 
-    private int recursivelyClimbStairs(int stepNumber, int numberOfStairs, int[] memo) {
-        if (stepNumber > numberOfStairs) {
-            return 0;
-        }
-        if (stepNumber == numberOfStairs) {
-            return 1;
-        }
-        if (memo[stepNumber] > 0) {
-            return memo[stepNumber];
-        }
-        memo[numberOfStairs] = recursivelyClimbStairs(numberOfStairs + 1, numberOfStairs, memo) +
-                recursivelyClimbStairs(numberOfStairs + 2, numberOfStairs, memo);
+    private long recursivelyClimbStairs(int stepNumber, int numberOfStairs, long[] memo) {
+        if (stepNumber > numberOfStairs) return 0;
+        if (stepNumber == numberOfStairs) return 1;
+        if (memo[stepNumber] > 0) return memo[stepNumber];
+
+        memo[stepNumber] =  recursivelyClimbStairs(stepNumber + 1, numberOfStairs, memo) +
+                recursivelyClimbStairs(stepNumber + 2, numberOfStairs, memo);
+
         return memo[stepNumber];
     }
 
     public static void main(String[] args) {
         StairClimbing stairClimbing = new StairClimbing();
-        System.out.println(stairClimbing.climbStairs(1));
+        System.out.println(stairClimbing.climbStairs(100));
     }
 }
