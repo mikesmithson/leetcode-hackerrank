8c12e57eaff02d5ee2d52b9597105ce98f94f2bc
Mike Smithson <mikesmithso@gmail.com>
2023-10-25 16:46:21 -0400

    add solutions

---
+++ src/main/java/com/discover/MissingNumberPartII.java
@@ -0,0 +1,26 @@
+package com.discover;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MissingNumberPartII {
+    public int missingNumber(int[] array) {
+        List<Integer> listOfNums = new ArrayList<>();
+        for (int j : array) {
+            listOfNums.add(j);
+        }
+        Collections.sort(listOfNums);
+        for (int i = 1; i < listOfNums.size(); i++) {
+            if (listOfNums.get(i) - listOfNums.get(i - 1) > 1) {
+                return listOfNums.get(i) - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        MissingNumberPartII missing = new MissingNumberPartII();
+        System.out.println(missing.missingNumber(new int[]{1, 2, 4, 6, 3, 7, 8}));
+    }
+}
